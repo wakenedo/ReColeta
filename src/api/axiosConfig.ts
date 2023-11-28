7d219bcef3991226d82eef736e47e7ccca04cdc2
wakenedo <a_nuner@outlook.com>
2023-11-28 18:06:11 -0300

    Update axiosConfig.ts

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-//const baseURL = process.env.NODE_ENV === 'production'
-//  ? process.env.REACT_APP_PRODUCTION_BASE_URL
-//  : process.env.REACT_APP_LOCAL_BASE_URL;
+const baseURL = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_PRODUCTION_BASE_URL
+  : process.env.REACT_APP_LOCAL_BASE_URL;
 
-const baseURL = process.env.REACT_APP_LOCAL_BASE_URL;
+
 
 // Create an instance of Axios with custom configurations
 const instance = axios.create({
