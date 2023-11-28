63b010bc9344cf4cbc78a65d2d0bd77509cac001
wakenedo <a_nuner@outlook.com>
2023-11-28 17:24:59 -0300

    Adapting for production

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_PRODUCTION_BASE_URL
+  : process.env.REACT_APP_LOCAL_BASE_URL;
+
 // Create an instance of Axios with custom configurations
 const instance = axios.create({
-  baseURL: 'http://localhost:8080', // Replace with your backend API URL
+  baseURL,
   timeout: 10000, // Adjust the timeout as needed
   headers: {
     'Content-Type': 'application/json',
