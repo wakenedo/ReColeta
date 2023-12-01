3e0931c41821c487e41b5fac94d349ca30e09f64
wakenedo <a_nuner@outlook.com>
2023-12-01 13:09:17 -0300

    RegistrationInterface  - Verificação
    Verificação do formato do email, comprimento da senha, funcionando em tempo real

--- src/components/RegistrationContainer/RegistrationInterface/index.tsx
+++ src/components/RegistrationContainer/RegistrationInterface/index.tsx
@@ -10,47 +10,61 @@ export const RegistrationInterface = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [userType, setUserType] = useState('');
-  const [error, setError] = useState<string | null>(null);
+  const [errors, setErrors] = useState<string[]>([]); // Updated state for multiple errors
   const navigate = useNavigate();
 
-  const handleSelectUserType = (userType: string) => {
-    setError(null);
-    setUserType(userType);
+  const handleSelectUserType = (selectedUserType: string) => {
+    // Clear previous errors
+    setErrors([]);
+
+    // Check for user type errors and update the error state
+    if (!selectedUserType) {
+      addError('Por favor, selecione um tipo de usuário.');
+    } else {
+      // Remove the user type error if a valid type is selected
+      setErrors(errors.filter((error) => error !== 'Por favor, selecione um tipo de usuário.'));
+    }
+
+    setUserType(selectedUserType);
   };
 
+  const addError = (error: string) => {
+    if (!errors.includes(error)) {
+      setErrors([...errors, error]);
+    }
+  };
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const handleRegistration = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!email.trim() || !password.trim() || !userType) {
-      setError('Por favor, preencha todos os campos com * e selecione um tipo de usuário.');
-      return;
+    // Clear previous errors
+    setErrors([]);
+
+
+
+    // Populate errors array for validation
+    if (!email.trim() || !emailRegex.test(email)) {
+      addError('Por favor, insira um endereço de e-mail válido.');
+    }
+    if (!password.trim() || password.length < 8) {
+      addError('A senha deve ter pelo menos 8 caracteres.');
     }
     if (!userType) {
-      setError('Por favor, selecione um tipo de usuário.');
+      addError('Por favor, selecione um tipo de usuário.');
+    }
+    // If there are errors, return and display them
+    if (errors.length > 0) {
       return;
     }
 
     try {
-      setError(null);
+      const response = await registerUser({ email, password, firstName, lastName, userType });
 
-      // Log the data before sending
-
-      const response = await registerUser({ email, password, firstName, lastName, userType })
-      console.log('Registration Data:', {
-        email,
-        password,
-        firstName,
-        lastName,
-        userType: response.data.userType || 'No User Type', // Access the name property
-        response: response.data.id,
-      });
-
-      // Store user ID in localStorage after successful registration
       localStorage.setItem('userId', String(response.data.id));
 
       navigate('/dashboard');
     } catch (error: any) {
-      setError('Erro ao cadastrar usuário. Por favor, tente novamente.');
       console.error('Registration error:', error.message);
     }
   };
@@ -80,8 +94,17 @@ export const RegistrationInterface = () => {
             type="text"
             placeholder="Registrar Email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className={error && !password.trim() ? 'error' : ''} // Highlight if error
+            onChange={(e) => {
+              setEmail(e.target.value);
+              // Check for email errors and update the error state
+              if (!e.target.value.trim() || !emailRegex.test(e.target.value)) {
+                addError('Por favor, insira um endereço de e-mail válido.');
+              } else {
+                // Remove the email error if the input is valid
+                setErrors(errors.filter((error) => error !== 'Por favor, insira um endereço de e-mail válido.'));
+              }
+            }}
+            className={errors.includes('Por favor, insira um endereço de e-mail válido.') ? 'error' : ''}
           />
         </div>
         <div>
@@ -90,8 +113,17 @@ export const RegistrationInterface = () => {
             type="password"
             placeholder="Registrar Senha"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={error && !password.trim() ? 'error' : ''} // Highlight if error
+            onChange={(e) => {
+              setPassword(e.target.value);
+              // Check for password errors and update the error state
+              if (!e.target.value.trim() || e.target.value.length < 8) {
+                addError('A senha deve ter pelo menos 8 caracteres.');
+              } else {
+                // Remove the password error if the input is valid
+                setErrors(errors.filter((error) => error !== 'A senha deve ter pelo menos 8 caracteres.'));
+              }
+            }}
+            className={errors.includes('A senha deve ter pelo menos 8 caracteres.') ? 'error' : ''}
           />
         </div>
         <div>
@@ -133,7 +165,14 @@ export const RegistrationInterface = () => {
             Registrar
           </button>
         </div>
-        {error && <div className="registration-warning">{error}</div>}
+        {/* Display multiple errors */}
+        {errors.length > 0 && (
+          <div className="registration-warning">
+            {errors.map((errMsg, index) => (
+              <p key={index}>{errMsg}</p>
+            ))}
+          </div>
+        )}
         <div className="registration-login-link">
           <Link to="/login">Já tem cadastro? Faça login aqui.</Link>
         </div>
